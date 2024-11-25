b8e833dc7dfe54cc9292370a27292feaf2dd9ffd
AustynT <AustynTravis9@gmail.com>
2024-11-24 17:09:54 -0700

    updated database.py file for postgresql

---
+++ app/db/database.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from the .env file
+load_dotenv()
+
+# Get the database URL from the .env file
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Initialize the SQLAlchemy engine
+engine = create_engine(DATABASE_URL)
+
+# Create a configured "Session" class
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base class for all models
+Base = declarative_base()
