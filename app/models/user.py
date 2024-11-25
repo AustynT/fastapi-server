c8db5d81c6cd77175227ebd0dac4a962a40e789a
AustynT <AustynTravis9@gmail.com>
2024-11-24 18:28:40 -0700

    built UserModel

---
+++ app/models/user.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from app.db.database import Base
+from datetime import datetime, timezone
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, nullable=False)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+    updated_at = Column(DateTime, default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
