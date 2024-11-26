dd5f12bdff183cf1085956873537d2f67b7d7526
AustynT <AustynTravis9@gmail.com>
2024-11-25 23:41:37 -0700

    updated user_model to use base_model

--- app/models/user.py
+++ app/models/user.py
@@ -1,13 +1,11 @@
 from sqlalchemy import Column, Integer, String, DateTime
-from app.db.database import Base
+from app.models.base_model import BaseModel
 from datetime import datetime, timezone
 
-class User(Base):
+class User(BaseModel):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, nullable=False)
     first_name = Column(String, nullable=False)
     last_name = Column(String, nullable=False)
-    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
-    updated_at = Column(DateTime, default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
