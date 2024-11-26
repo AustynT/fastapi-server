0d1bd15826c339c0579eea7e893420931c1a17ed
AustynT <AustynTravis9@gmail.com>
2024-11-26 00:44:32 -0700

    fixed model issues

--- app/models/service.py
+++ app/models/service.py
@@ -6,7 +6,7 @@ class Service(BaseModel):
    __tablename__ = "services" 
 
    serive_id = Column(Integer, primary_key = True, index = True)
-   user_id = Column(Integer, ForeignKey("users.id", nullable= False, index= True))
+   user_id = Column(Integer, ForeignKey("users.id"), nullable= False, index= True)
    service_name = Column(String, unique = True, nullable = False)
    total_amount = Column(Float, nullable = False)
 
