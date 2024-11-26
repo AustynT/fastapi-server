ef85a452c00433c3d7bd5869b5271fb9049df2ca
AustynT <AustynTravis9@gmail.com>
2024-11-26 00:42:37 -0700

    updated BaseModel

--- app/models/base_model.py
+++ app/models/base_model.py
@@ -1,8 +1,15 @@
-from sqlalchemy.ext.declarative import as_declarative
 from sqlalchemy import Column, DateTime
+from sqlalchemy.ext.declarative import declared_attr
+from app.db.database import Base
 from datetime import datetime, timezone
 
-@as_declarative()
-class BaseModel:
-    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc), nullable=False)
-    updated_at = Column(DateTime, default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc), nullable=False)
+class BaseModel(Base):
+    __abstract__ = True  # Mark this class as abstract (no table will be created for it)
+
+    @declared_attr
+    def created_at(cls):
+        return Column(DateTime, default=lambda: datetime.now(timezone.utc), nullable=False)
+
+    @declared_attr
+    def updated_at(cls):
+        return Column(DateTime, default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc), nullable=False)
