158ed2ec756323a2adb18d529348277e12594d0d
AustynT <AustynTravis9@gmail.com>
2024-11-30 16:26:27 -0700

    added hash_password to model

--- app/models/user.py
+++ app/models/user.py
@@ -7,6 +7,7 @@ class User(BaseModel):
 
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
     first_name = Column(String, nullable=False)
     last_name = Column(String, nullable=False)
     
