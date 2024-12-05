fce0daa2430cbf566728d19f259903ebe9ce07ac
AustynT <AustynTravis9@gmail.com>
2024-12-05 00:51:22 -0700

    removed create user from endpoint

--- app/api/endpoints/users.py
+++ app/api/endpoints/users.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from typing import List
 from app.db.database import get_db
-from app.utils.database_utils import add_and_commit, find_and_update, get_instance_by_id, delete_and_commit
+from app.utils.database_utils import find_and_update, get_instance_by_id, delete_and_commit
 from app.models.user import User
 
 router = APIRouter()
@@ -13,14 +13,6 @@ async def get_users(db: Session = Depends(get_db)):
     Get a list of all users.
     """
     return db.query(User).all()
-
-@router.post("/users", response_model=dict)
-async def create_user(user_data: dict, db: Session = Depends(get_db)):
-    """
-    Create a new user.
-    """
-    new_user = User(**user_data)
-    return add_and_commit(db, new_user)
 
 @router.put("/users/{user_id}", response_model=dict)
 async def update_user(user_id: int, updated_data: dict, db: Session = Depends(get_db)):
