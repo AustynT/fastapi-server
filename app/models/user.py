55103aae0b4eb2252872f3e5f89dc496134035f7
AustynT <AustynTravis9@gmail.com>
2024-12-07 17:03:48 -0700

    added index to users model

--- app/models/user.py
+++ app/models/user.py
@@ -6,7 +6,7 @@ class User(BaseModel):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, nullable=False)
+    email = Column(String, unique=True, nullable=False, index=True)
     hashed_password = Column(String, nullable=False)
     first_name = Column(String, nullable=False)
     last_name = Column(String, nullable=False)
