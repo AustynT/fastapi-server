af1fd1915b877e192cd793ef82413f8212d2d860
AustynT <AustynTravis9@gmail.com>
2024-12-14 21:59:30 -0700

    updated config to have refresh days

--- app/core/config.py
+++ app/core/config.py
@@ -16,6 +16,8 @@ class Config:
     ALGORITHM: str = os.getenv("ALGORITHM", "HS256")
     ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
 
+    # Refresh token expiration setting (in days)
+    REFRESH_TOKEN_EXPIRE_DAYS: int = int(os.getenv("REFRESH_TOKEN_EXPIRE_DAYS", 7))
     # CORS settings
     ALLOWED_ORIGINS: list = os.getenv("ALLOWED_ORIGINS", "http://localhost:3000,http://127.0.0.1:3000").split(",")
 
