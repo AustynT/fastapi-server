38338dce7374f7cbcd29329bd6c5fef2d1fa23f3
AustynT <AustynTravis9@gmail.com>
2024-12-15 13:25:39 -0700

    created job history routes

--- app/api/endpoints/job_history.py
+++ app/api/endpoints/job_history.py
@@ -5,14 +5,14 @@ from app.db.database import get_db
 from app.db.dependency import get_current_user
 from app.models.job_history import JobHistory
 from app.models.user import User
-from app.schemas.job_history import JobHistoryResponse
+from app.schemas.job_history import JobHistoryResponse, JobHistoryRequest
 router = APIRouter()
 
-@router.get("/job-history/{user_id}", response_model=List[JobHistoryResponse])
+@router.get("/job-history", response_model=List[JobHistoryResponse])
 async def get_user_jobs(
         user_id: int,
         db: Session = Depends(get_db), 
-        current_user: User = Depends(get_current_user)):
+    ):        
     
     user_jobs = db.query(JobHistory).filter(JobHistory.user_id == user_id).all()
     
@@ -20,3 +20,20 @@ async def get_user_jobs(
         raise HTTPException(status_code=404, detail="No Job History Found for this user")
     
     return user_jobs
+
+@router.post("/create-job-history", response_model=JobHistoryResponse)
+async def create_job_history(job_history_data: JobHistoryRequest, 
+                             db: Session = Depends(get_db),
+                             current_user: User = Depends(get_current_user)):
+    pass
+
+
+@router.put('/edit-job-history/{job_history_id}', response_model=JobHistoryResponse)
+async def edit_job_history(job_data: JobHistoryRequest,
+                           db: Session = Depends(get_db),
+                           current_user: User = Depends(get_current_user)):
+    pass
+
+@router.delete("/delete-job-history/{job_history_id}")
+async def delete_job_history():
+    pass
