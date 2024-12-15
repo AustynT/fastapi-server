b82de1f55a00bcf1c32b838b9966c4d059675e55
AustynT <AustynTravis9@gmail.com>
2024-12-14 21:33:56 -0700

    updated basemodel to have to_dict

--- app/models/base_model.py
+++ app/models/base_model.py
@@ -1,42 +1,43 @@
 from sqlalchemy import Column, DateTime
 from sqlalchemy.ext.declarative import declared_attr
-from app.db.database import Base
 from datetime import datetime, timezone
+from app.db.database import Base
 
 
 class BaseModel(Base):
-    __abstract__ = True  # This class is abstract and will not create its own table
+    __abstract__ = True  # Ensure this doesn't create a table
 
     @declared_attr
     def created_at(cls):
         """
-        Timestamp for when the record is created.
-        Defaults to the current UTC time.
+        Timestamp for when the record is created. Defaults to the current UTC time.
         """
         return Column(
             DateTime, 
-            default=lambda: datetime.now(timezone.utc), 
+            default=datetime.now(timezone.utc), 
             nullable=False
         )
 
     @declared_attr
     def updated_at(cls):
         """
-        Timestamp for when the record is last updated.
-        Automatically updated to the current UTC time on record updates.
+        Timestamp for when the record is last updated. Automatically updates to the current UTC time.
         """
         return Column(
             DateTime,
-            default=lambda: datetime.now(timezone.utc),
-            onupdate=lambda: datetime.now(timezone.utc),
+            default=datetime.now(timezone.utc),
+            onupdate=datetime.now(timezone.utc),
             nullable=False
         )
 
+    def to_dict(self):
+        """
+        Convert model instance to a dictionary for serialization.
+        """
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
 
     def __repr__(self):
-        column_values = {
-            column.name: getattr(self, column)
-            for column in self.__table__.columns
-        }
-        
-        return f"<{self.__class__.__name__} {column_values}"
+        """
+        Provide a readable string representation of the model.
+        """
+        return f"<{self.__class__.__name__} {self.to_dict()}>"
